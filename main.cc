1bce33674bfdbe401b05c595d4cbb805d3b1374a
CzarsonZiomal <szwedekc@gmail.com>
2022-12-04 11:21:28 +0100

    working tabu

--- main.cc
+++ main.cc
@@ -6,6 +6,7 @@
 #include <random>
 #include <sstream>
 #include <vector>
+#include <chrono>
 
 using namespace std;
 
@@ -20,6 +21,14 @@ int random(int lower_bound, int higher_bound) {
     mt19937 rng(dev());
     uniform_int_distribution<mt19937::result_type> dist(lower_bound, higher_bound);
     return dist(rng);
+}
+
+double distance_sum(vector<Truck> solution) {
+    double distance = 0;
+    for (int i = 0; i < solution.size(); i++) {
+        distance += solution[i].act_time;
+    }
+    return distance;
 }
 
 void load_file_contents(ifstream &file, vector<string> &file_content) {
@@ -37,6 +46,8 @@ void write_to_file(ofstream &output_file, vector<Truck> &trucks) {
         for (int i = 0; i < trucks.size(); i++) {
             distance += trucks[i].act_time;
         }
+        cout << "WRITE TO FILE: " << trucks.size() << " " << fixed << setprecision(5) << distance << endl;
+        // cout << "Distance sum: " << distance_sum(trucks) << endl;
         output_file << trucks.size() << " " << fixed << setprecision(5) << distance << endl;
         for (int i = 0; i < trucks.size(); i++) {
             for (int j = 0; j < trucks[i].customers.size(); j++) {
@@ -151,40 +162,32 @@ void find_answer(vector<Customer> &customers, vector<vector<float>> &matrix, vec
     trucks[trucks.size() - 1].act_time += matrix[trucks[trucks.size() - 1].act_location][0];
 }
 
-float distance_sum(vector<Truck> solution) {
-    float distance = 0;
-    for (int i = 0; i < solution.size(); i++) {
-        distance += solution[i].act_time;
+
+void sort_neighbourhood(vector<vector<Truck>> &neighbourhood) {
+    vector<vector<Truck>> prev_neighbourhood;
+    for (int i = 0; i < neighbourhood.size(); i++) {
+        prev_neighbourhood.push_back(neighbourhood[i]);
     }
-    return distance;
+    neighbourhood.clear();
+    for (int i = 0; i < prev_neighbourhood.size(); i++) {
+        int min_trucks = 9999999;
+        float min_distance = 9999999;
+        int index = 0;
+        for (int j = 0; j < prev_neighbourhood[i].size(); j++) {
+            if (prev_neighbourhood[i].size() < min_trucks) {
+                min_trucks = prev_neighbourhood[i].size();
+                min_distance = distance_sum(prev_neighbourhood[i]);
+                index = i;
+            } else if (prev_neighbourhood.size() == min_trucks && distance_sum(prev_neighbourhood[i]) < min_distance) {
+                min_distance = distance_sum(prev_neighbourhood[i]);
+                index = i;
+            }
+        }
+        neighbourhood.push_back(prev_neighbourhood[index]);
+        prev_neighbourhood.erase(prev_neighbourhood.begin() + index);
+    }
+    return;
 }
-
-// void sort_neighbourhood(vector<vector<Truck>> &neighbourhood) {
-//     vector<vector<Truck>> prev_neighbourhood;
-//     for (int i = 0; i < neighbourhood.size() - 1; i++) {
-//         prev_neighbourhood.push_back(neighbourhood[i]);
-//     }
-
-//     neighbourhood.clear();
-//     for (int i = 0; i < prev_neighbourhood.size() - 1; i++) {
-//         int min_trucks = 9999999;
-//         float min_distance = 9999999;
-//         int index = 0;
-//         for (int j = 0; j < prev_neighbourhood[i].size(); j++) {
-//             if (prev_neighbourhood[i].size() < min_trucks) {
-//                 min_trucks = prev_neighbourhood[i].size();
-//                 min_distance = distance_sum(prev_neighbourhood[i]);
-//                 index = i;
-//             } else if (prev_neighbourhood.size() == min_trucks && distance_sum(prev_neighbourhood[i]) < min_distance) {
-//                 min_distance = distance_sum(prev_neighbourhood[i]);
-//                 index = i;
-//             }
-//         }
-//         neighbourhood.push_back(prev_neighbourhood[index]);
-//         prev_neighbourhood.erase(prev_neighbourhood.begin() + index);
-//     }
-//     return;
-// }
 
 bool compare_solutions(vector<Truck> t1, vector<Truck> t2) {
     if (t1.size() > t2.size()) {
@@ -196,15 +199,15 @@ bool compare_solutions(vector<Truck> t1, vector<Truck> t2) {
     return false;
 }
 
-void get_neighbourhood(vector<Customer> &customers, vector<vector<float>> &matrix, vector<Truck> current_solution, vector<vector<Truck>> &neighbourhood, int capacity) {
+void get_neighbourhood(vector<Customer> &customers, vector<vector<float>> &matrix, vector<Truck> &current_solution, vector<vector<Truck>> &neighbourhood, int capacity) {
     int vehicle_nr = random(0, current_solution.size() - 1);
-    int customer_nr = random(0, current_solution[vehicle_nr].customers.size() - 1);
-
-    for (int i = 0; i < current_solution.size() - 1; i++) {
+    int customer_nr_on_route = random(0, current_solution[vehicle_nr].customers.size() - 1);
+    int customer_nr = current_solution[vehicle_nr].customers[customer_nr_on_route];
+    for (int i = 0; i < current_solution.size(); i++) {
         if (i == vehicle_nr) {
             continue;
         }
-        if (customers[customer_nr].demand + current_solution[i].act_capacity < capacity) {
+        if (customers[customer_nr].demand > current_solution[i].act_capacity) {
             continue;
         }
 
@@ -213,11 +216,11 @@ void get_neighbourhood(vector<Customer> &customers, vector<vector<float>> &matri
         vector<Truck> possible_solution;
         Truck possible_new_truck(capacity);
         bool pushed = false;
-        for (int j = 0; j < current_solution[i].customers.size() - 1; j++) {
+        for (int j = 0; j < current_solution[i].customers.size(); j++) {
             if (!pushed) {
-                real_time += max(matrix[current_position][j], (float)customers[j].ready_time - real_time) + customers[j].service_time;
+                real_time += max(matrix[current_position][current_solution[i].customers[j]], (float)customers[current_solution[i].customers[j]].ready_time - real_time) + customers[current_solution[i].customers[j]].service_time;
                 possible_time += max(matrix[current_position][customer_nr], (float)customers[customer_nr].ready_time - possible_time) + customers[customer_nr].service_time;
-                possible_time += max(matrix[customer_nr][j], (float)customers[j].ready_time - possible_time) + customers[j].service_time;
+                possible_time += max(matrix[customer_nr][j], (float)customers[current_solution[i].customers[j]].ready_time - possible_time) + customers[current_solution[i].customers[j]].service_time;
                 if (possible_time <= real_time) {
                     possible_new_truck.customers.push_back(customer_nr);
                     pushed = true;
@@ -226,22 +229,62 @@ void get_neighbourhood(vector<Customer> &customers, vector<vector<float>> &matri
                 }
                 current_position = current_solution[i].customers[j];
             }
-            possible_new_truck.customers.push_back(j);
+            possible_new_truck.customers.push_back(current_solution[i].customers[j]);
         }
 
         if (pushed) {
-            for (int j = 0; j < current_solution.size() - 1; j++) {
+            for (int j = 0; j < current_solution.size(); j++) {
                 if (i != j) {
                     possible_solution.push_back(current_solution[j]);
                 } else {
                     possible_solution.push_back(possible_new_truck);
                 }
             }
-            neighbourhood.push_back(possible_solution);
+            possible_solution[vehicle_nr].customers.erase(possible_solution[vehicle_nr].customers.begin() + customer_nr_on_route);
+            
+            // changing act_time in line where customer was deleted and where customer was put and act_capacity
+            bool validate_route = true;
+
+            possible_solution[vehicle_nr].act_time = 0;
+            possible_solution[vehicle_nr].act_capacity = capacity;
+            current_position = 0;
+            for (int j = 0; j < possible_solution[vehicle_nr].customers.size(); j++) {
+                possible_solution[vehicle_nr].act_time += max(matrix[current_position][possible_solution[vehicle_nr].customers[j]], customers[possible_solution[vehicle_nr].customers[j]].ready_time - possible_solution[vehicle_nr].act_time) + customers[possible_solution[vehicle_nr].customers[j]].service_time;
+                possible_solution[vehicle_nr].act_capacity -= customers[possible_solution[vehicle_nr].customers[j]].demand;
+                current_position = possible_solution[vehicle_nr].customers[j];
+            }
+            possible_solution[vehicle_nr].act_time += matrix[current_position][0];
+
+            possible_solution[i].act_time = 0;
+            possible_solution[i].act_capacity = capacity;
+            current_position = 0;
+            for (int j = 0; j < possible_solution[i].customers.size(); j++) {
+                possible_solution[i].act_time += max(matrix[current_position][possible_solution[i].customers[j]], customers[possible_solution[i].customers[j]].ready_time - possible_solution[i].act_time) + customers[possible_solution[i].customers[j]].service_time;
+                possible_solution[i].act_capacity -= customers[possible_solution[i].customers[j]].demand;
+                if (possible_solution[i].act_time - customers[possible_solution[i].customers[j]].service_time > customers[possible_solution[i].customers[j]].due_date) {
+                    validate_route = false;
+                }
+                current_position = possible_solution[i].customers[j];
+            }
+            possible_solution[i].act_time += matrix[current_position][0];
+            if (possible_solution[i].act_time > customers[0].due_date) {
+                validate_route = false;
+            }
+
+            if (possible_solution[vehicle_nr].customers.size() == 0) {
+                // cout << "Empty truck" << endl;
+                possible_solution.erase(possible_solution.begin() + vehicle_nr);
+            }
+            
+
+            if (validate_route) {
+                neighbourhood.push_back(possible_solution);
+            }
         }
     }
     return;
 }
+
 
 bool in_tabu2(vector<vector<Truck>> &tabu_list, vector<Truck> &solution) {
     for (vector<Truck> tabu : tabu_list) {
@@ -260,7 +303,7 @@ bool in_tabu2(vector<vector<Truck>> &tabu_list, vector<Truck> &solution) {
 }
 
 bool in_tabu(vector<vector<Truck>> &tabu_list, vector<Truck> &solution) {
-    for (int i = 0; i < tabu_list.size() - 1; i++) {
+    for (int i = 0; i < tabu_list.size(); i++) {
         bool is_member = true;
         if (tabu_list[i].size() != solution.size()) {
             continue;
@@ -292,20 +335,24 @@ void tabu_search(vector<Customer> &customers, vector<vector<float>> &matrix, vec
     vector<vector<Truck>> neigbourhood;
     vector<Truck> best_solution;
     vector<Truck> current_solution;
-    for (int i = 0; i < trucks.size() - 1; i++) {
+    for (int i = 0; i < trucks.size(); i++) {
         best_solution.push_back(trucks[i]);
         current_solution.push_back(trucks[i]);
     }
+
     vector<vector<Truck>> tabu_list;
 
     int how_many = 1;
+    write_to_file(output_file, best_solution);
 
-    while (how_many > 0) {
+    while (true) {
         get_neighbourhood(customers, matrix, current_solution, neigbourhood, capacity);
-        sort(neigbourhood.begin(), neigbourhood.end(), compare_solutions);
-        // sort_neighbourhood(neigbourhood);
+        // sort(neigbourhood.begin(), neigbourhood.end(), compare_solutions);
+        // cout << "Sort" << endl;
+        sort_neighbourhood(neigbourhood);
+        // cout << "Sorted" << endl;
         vector<Truck> best_candidate;
-        for (int i = 0; i < neigbourhood.size() - 1; i++) {
+        for (int i = 0; i < neigbourhood.size(); i++) {
             best_candidate.clear();
             if (!in_tabu(tabu_list, neigbourhood[i])) {
                 for (int j = 0; j < neigbourhood[i].size(); j++) {
@@ -315,31 +362,38 @@ void tabu_search(vector<Customer> &customers, vector<vector<float>> &matrix, vec
             }
         }
         if (!best_candidate.empty()) {
+            // cout << "Found candidate" << endl;
             tabu_list.push_back(best_candidate);
             if (tabu_list.size() > tabu_size) {
                 tabu_list.erase(tabu_list.begin());
             }
             if (new_solution_better(best_solution, best_candidate)) {
+                // cout << "New solution is better" << endl;
                 best_solution.clear();
                 current_solution.clear();
-                for (int i = 0; i < best_candidate.size() - 1; i++) {
+                for (int i = 0; i < best_candidate.size(); i++) {
                     best_solution.push_back(best_candidate[i]);
                     current_solution.push_back(best_candidate[i]);
                 }
+                output_file.close();
+                output_file.open("tabu.txt");
+                write_to_file(output_file, best_solution);
             } else {
-                current_solution.clear();
-                for (int i = 0; i < best_candidate.size() - 1; i++) {
-                    current_solution.push_back(best_candidate[i]);
-                }
+                // Po zaczeciu brania gorszych ma czasem problem znalezc lepsze
+                // cout << "New solution is worse" << endl;
+                // current_solution.clear();
+                // for (int i = 0; i < best_candidate.size(); i++) {
+                //     current_solution.push_back(best_candidate[i]);
+                // }
             }
-            write_to_file(output_file, best_solution);
+            how_many--;
         }
         neigbourhood.clear();
-        how_many--;
     }
 }
 
 int main(int argc, char *argv[]) {
+    auto start = std::chrono::high_resolution_clock::now();
     if (argc != 3) {
         cout << "usage: " << argv[0] << " dataset_file output_file" << endl;
         return 1;
@@ -378,14 +432,19 @@ int main(int argc, char *argv[]) {
     find_answer(customers, matrix, trucks, capacity);
     write_to_file(output_file, trucks);
 
-    int tabu_size = 2;
+    int tabu_size = 10;
 
     ofstream output_tabu;
     output_tabu.open("tabu.txt");
 
     tabu_search(customers, matrix, trucks, capacity, output_tabu, tabu_size);
-    output_tabu.close();
-
+    if (output_tabu.is_open()) {
+        output_tabu.close();
+    }
+    cout << "SIMEA" << endl;
+    auto elapsed = std::chrono::high_resolution_clock::now() - start;
+    long long microseconds = std::chrono::duration_cast<std::chrono::seconds>(elapsed).count();
+    cout << "Czas: " << microseconds;
     input_file.close();
     output_file.close();
 }
